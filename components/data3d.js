9ee9d33a06bac28680b054e1e95880c7bb9100c3
dhaanutyan <victoria.onishuk@gmail.com>
2017-06-07 23:32:18 +0200

    sa123

--- components/data3d.js
+++ components/data3d.js
@@ -18,8 +18,8 @@
 
     update: function () {
       var this_ = this
-      var url = this_.data.url || this_.data.URL
-      var key = this_.data.key || this_.data.KEY
+      var url = this_.data.url
+      var key = this_.data.key
 
       // check params
       if ((!url || url === '') && (!key || key === '')) return
@@ -30,10 +30,9 @@
       // create new one
       this_.mesh = new THREE.Object3D()
       this_.data3dView = new BASE.three.Data3dView({ parent: this_.mesh })
-      this.el.data3dView = this.data3dView
+      
       // load 3d file
       ;(key ? BASE.store.get(key) : BASE.data3d.load(url)).then(function(data3d){
-        this.el.data3d = data3d
         // update view
         this_.data3dView.set(data3d)
         this_.el.setObject3D('mesh', this_.mesh)
